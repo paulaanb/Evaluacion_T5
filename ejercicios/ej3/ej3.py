07cf76385fde4587c53c69c1b807f524d0225ed0
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-12-05 16:54:02 +0100

    gestor borrar

--- ejercicios/ej3/ej3.py
+++ ejercicios/ej3/ej3.py
@@ -22,5 +22,12 @@ class Gestor:
             return
         self.personajes.append(p)
         self.guardar()
-
+    def borrar(self, nombre):
+        for pTemp in self.personajes:
+            if pTemp.nombre == nombre:
+                self.personajes.remove(pTemp)
+                self.guardar()
+                print("\nEl personaje {} ha sido borrado".format(nombre))
+                return
+                
 
