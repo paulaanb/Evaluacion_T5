f1937b6eb4cb383a43a30710e74f4fec516aaf13
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-12-06 13:46:15 +0100

    correccion ejercicio 4

--- ejercicios/ej4/ej4.py
+++ ejercicios/ej4/ej4.py
@@ -1,19 +1,19 @@
-import plataform
+import platform
 import os
 import time
-import datatime
+import datetime
 
 def limpiar_pantalla():
     time.sleep(2)
 
-        if platform.system() == 'Windows':
-            os.system('cls')
-        else:
-            os.system('clear')
-if __name__ == '__main__':
+    if platform.system() == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+if _name_ == '_main_':
     limpiar_pantalla()
 
-fecha_actual = datatime.datatime.now()
+fecha_actual = datetime.datetime.now()
 
 fecha_actual
 print(type(fecha_actual))
